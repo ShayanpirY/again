a0240766a2552e100e21aff2d18df343b05f1f4c
ShayanpirY <pir0918021@gmail.com>
2026-08-28 14:07:27 +0330

    حذف استوری

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,15 +1,11 @@
 import HeroBanners from '@/components/home/HeroBanners';
-import { StoriesBar } from '@/components/home/StoriesBar';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-white">
-      {/* استوری‌های فعال زیر هدر */}
-      <StoriesBar />
-
       {/* بنرهای جدید شبیه سایت Mayoral */}
       <HeroBanners />
-      
+
       {/* بقیه سکشن‌های صفحه اصلی در آینده اینجا قرار می‌گیرند */}
     </main>
   );
